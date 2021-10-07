95aa63310162877ba6d10512c18c7ff6be83c979
Kislay raj <kislayraj1234@gmail.com>
2021-10-07 20:43:03 +0530

    banner component  was added to TvPage.jsx

--- src/pages/TvPage.jsx
+++ src/pages/TvPage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import TvList from '../components/Tv/TvList';
 import TvTopRated from '../components/Tv/TvTopRated';
 import TvTrendings from '../components/Tv/TvTrending';
+import Banner from '../components/banner/Banner';
 
 const TvPage = () => {
   return (
     <>
+      <Banner />
       <TvList />
       <TvTopRated />
       <TvTrendings />
